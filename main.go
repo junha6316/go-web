7bbb41a3627292374f2f4bf04078fe1c60812f43
junha6316 <junha6316@gmail.com>
2021-12-05 23:47:33 +0900

    modify: Context를 사용하도록 수정

--- main.go
+++ main.go
@@ -7,31 +7,31 @@ import (
 
 func main() {
 
-	r := &router{make(map[string]map[string]http.HandlerFunc)}
+	r := &router{make(map[string]map[string]HandlerFunc)}
 
-	r.HandleFunc("GET", "/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "welcome!")
+	r.HandleFunc("GET", "/", func(c *Context) {
+		fmt.Fprintln(c.ResponseWriter, "welcome!")
 	})
 
-	r.HandleFunc("GET", "/about", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "About!")
+	r.HandleFunc("GET", "/about", func(c *Context) {
+		fmt.Fprintln(c.ResponseWriter, "About!")
 	})
 
-	r.HandleFunc("GET", "/users/:id", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "retrieve user!")
+	r.HandleFunc("GET", "/users/:id", func(c *Context) {
+		fmt.Fprintln(c.ResponseWriter, "retrieve user!", c.Params["id"])
 	})
 
-	r.HandleFunc("GET", "/users/:user_id/addresses/:address_id", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "retrieve user's address!")
+	r.HandleFunc("GET", "/users/:user_id/addresses/:address_id", func(c *Context) {
+		fmt.Fprintln(c.ResponseWriter, "retrieve user's address!", c.Params["user_id"], c.Params["id"])
 	})
 
-	r.HandleFunc("POST", "/users", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "create user")
+	r.HandleFunc("POST", "/users", func(c *Context) {
+		fmt.Fprintln(c.ResponseWriter, "create user")
 	})
 
-	r.HandleFunc("POST", "/users/:user_id/addresses", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "create user's address")
+	r.HandleFunc("POST", "/users/:user_id/addresses", func(c *Context) {
+		fmt.Fprintln(c.ResponseWriter, "create user's address")
 	})
 
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":8000", r)
 }
